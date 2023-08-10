cdc2432d5cf0ba8a7cf097a41ebccd6cce953483
mjesusoj <mjesusoj@proton.me>
2023-08-10 20:33:21 +0200

    Add Aptabase analytics, privacy first and test with HomeScreen :rocket:

--- src/CraftianosMoney.jsx
+++ src/CraftianosMoney.jsx
@@ -3,6 +3,9 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { AppRouter } from './routes/AppRouter';
 
+import { init } from "@aptabase/web";
+init("A-EU-1480154143");
+
 export const CraftianosMoney = () => {  
     return (
         <Provider store={ store }>
